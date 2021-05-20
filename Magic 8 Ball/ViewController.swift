562fe39354d9afd05d3aadc3636aa51f92de226e
micmic17 <mickale_saturre@ligph.com>
2021-05-20 11:04:35 +0800

    Modify UI

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -12,7 +12,25 @@ class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
+    @IBOutlet weak var askLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var askButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-
+        askLabel.text = "Ask me anything"
+        askLabel.textColor = .white
+        askLabel.font = askLabel.font.withSize(20)
+        askLabel.textAlignment = .center
+        
+        askButton.setTitle("Ask", for: .normal)
+        askLabel.textColor = .white
+    }
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        imageView.image = ballArray[Int.random(in: 0...4)]
+    }
 }
 
